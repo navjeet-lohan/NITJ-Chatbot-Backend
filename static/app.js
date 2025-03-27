947bb04f826abe2cec4543c47bd6f3f179c9b985
Navjeet Lohan <navjeetlohan@gmail.com>
2025-03-27 14:16:38 +0530

    app.js updated

--- static/app.js
+++ static/app.js
@@ -74,8 +74,8 @@ class Chatbox {
     
         let msg1 = { name: "User", message: text1 };
         this.messages.push(msg1);
-    
-        fetch('https://nitj-chatbot-backend.onrender.com/predict', {
+        fetch('http://127.0.0.1:5000/predict', {
+        //fetch('https://nitj-chatbot-backend.onrender.com/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
